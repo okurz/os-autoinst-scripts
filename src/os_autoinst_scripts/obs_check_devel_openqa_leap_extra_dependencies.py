7a0fed1027c48fde622abb2586effd76c9fce778
Oliver Kurz <okurz@suse.de>
2025-12-04 13:32:28 +0100

    feat(obs-check-devel-openqa-leap-extra-dependencies): Convert bash to python

---
+++ src/os_autoinst_scripts/obs_check_devel_openqa_leap_extra_dependencies.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# Copyright SUSE LLC
+"""
+The script checks for a "Reason for linking:" comment in OBS packages.
+"""
+from typing import List
+
+import typer
+from rich.console import Console
+from sh import osc
+
+app = typer.Typer()
+console = Console()
+
+
+@app.command()
+def main() -> None:
+    """
+    Check for a "Reason for linking:" comment in OBS packages.
+    """
+    osc_cmd = osc.bake("--apiurl", "https://api.opensuse.org")
+    problem = False
+    projects = osc_cmd.search("--project", "-s", "devel:openQA:Leap:").stdout.decode().splitlines()
+    for project in projects:
+        project = project.strip()
+        if not project.startswith("devel:openQA:Leap:"):
+            continue
+        packages = osc_cmd.list(project).stdout.decode().splitlines()
+        for package in packages:
+            package = package.strip()
+            try:
+                comments = osc_cmd.api(f"/comments/package/{project}/{package}")
+                if "Reason for linking:" not in comments.stdout.decode():
+                    console.print(
+                        f"No reason for {project}/{package}, consider adding a comment or remove the link (see poo#128087 for details)",
+                        style="red",
+                    )
+                    problem = True
+            except Exception as e:
+                console.print(
+                    f"Error checking {project}/{package}: {e}",
+                    style="red",
+                )
+                problem = True
+    if problem:
+        raise typer.Exit(1)
+
+
+if __name__ == "__main__":
+    app()
