00cbfc9eaf821052ed0542964ca64938cb6982f3
Oliver Kurz <okurz@suse.de>
2025-12-04 13:38:29 +0100

    feat(openqa-label-known-issues-hook): Convert bash to python

---
+++ tests/test_openqa_label_known_issues_hook.py
@@ -0,0 +1,23 @@
+# Copyright SUSE LLC
+from unittest.mock import call
+
+from pytest_mock import MockerFixture
+from typer.testing import CliRunner
+
+from os_autoinst_scripts.openqa_label_known_issues_hook import app
+
+runner = CliRunner()
+
+
+def test_label_known_issues_hook(mocker: MockerFixture) -> None:
+    mock_run = mocker.patch("subprocess.run")
+
+    result = runner.invoke(app, ["12345"])
+
+    assert result.exit_code == 0
+    mock_run.assert_called_once_with(
+        ["openqa-label-known-issues-multi"],
+        input="https://openqa.opensuse.org/tests/12345",
+        text=True,
+        check=True,
+    )
