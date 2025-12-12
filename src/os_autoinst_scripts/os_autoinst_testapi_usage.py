40edc615dc7acd86388fd459f897eff7e0aa1ec9
Oliver Kurz <okurz@suse.de>
2025-12-12 16:13:49 +0100

    fix(os_autoinst_testapi_usage): Fix style issues

--- src/os_autoinst_scripts/os_autoinst_testapi_usage.py
+++ src/os_autoinst_scripts/os_autoinst_testapi_usage.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 # Copyright SUSE LLC
-"""
-The script uses ripgrep to find the usage of testapi.pm functions in a given repository.
-"""
-from typing import List
+"""The script uses ripgrep to find the usage of testapi.pm functions in a given repository."""
+import pathlib
 
 import typer
 from rich.console import Console
-from sh import rg, grep, cut, sort
+from sh import cut, grep, rg, sort
 
 app = typer.Typer()
 console = Console()
 
 
-def get_function_list(testapi_path: str) -> List[str]:
+def get_function_list(testapi_path: str) -> list[str]:
     try:
         functions = sort(
             grep(
                 cut(
-                    grep("^sub \w+\s*[({:]", testapi_path),
+                    grep(r"^sub \w+\s*[({:]", testapi_path),
                     "-f2",
                     "-d ",
                 ),
@@ -36,17 +34,15 @@ def main(
     testapi_path: str = typer.Argument(..., help="Path to testapi.pm"),
     repositories: List[str] = typer.Argument(..., help="Path to code repository"),
 ) -> None:
-    """
-    Use ripgrep to find the usage of testapi.pm functions in a given repository.
-    """
+    """Use ripgrep to find the usage of testapi.pm functions in a given repository."""
     if not testapi_path.endswith("testapi.pm"):
         testapi_path = f"{testapi_path}/testapi.pm"
 
     try:
-        with open(testapi_path, "r") as f:
+        with pathlib.Path(testapi_path).open(encoding="utf-8") as f:
             if "package testapi" not in f.read():
                 console.print(
-                    f"Warning: provided file does not look like testapi.pm",
+                    "Warning: provided file does not look like testapi.pm",
                     style="yellow",
                 )
     except FileNotFoundError:
